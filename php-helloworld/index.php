1b8579c1b5ce2a449bb4b6213e063c159562ddd8
Travis Michette <tmichett@redhat.com>
2021-06-08 14:56:55 -0400

    Update index.php
    Updated

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-echo "<br>"
-print "A change is in the air! \n";
+echo nl2br("Hello, World! php version is " . PHP_VERSION . "\n");
+print "A change is in the air! \r\n";
 ?>
